2eb14a0a5d901cff9919dcce99668d49ccc59757
Sneha Yadav <snyadav@iu.edu>
2025-10-20 01:42:25 +0000

    assignment week 8

--- apputil.py
+++ apputil.py
@@ -1,23 +1,44 @@
 from collections import defaultdict
-
+import re
+import random
+import numpy as np
 
 class MarkovText(object):
 
     def __init__(self, corpus):
-        self.corpus = corpus
-        self.term_dict = None  # you'll need to build this
+
+        self.corpus = re.findall(r"\b\w+\b", corpus)
+        self.term_dict = defaultdict(list)
 
     def get_term_dict(self):
+        """ This function creates dictionary of text """
+        for i in range(len(self.corpus) - 1):
+            current_word = self.corpus[i]
+            next_word = self.corpus[i + 1]
+            self.term_dict[current_word].append(next_word)
+        return dict(self.term_dict)
 
-        # your code here ...
-
-        self.term_dict = {}
-
-        return None
+   
 
 
     def generate(self, seed_term=None, term_count=15):
+        """ This function generates text based on seed term provisioning """
+        if seed_term is None:
+            seed_term = random.choice(list(self.term_dict.keys()))
+        elif seed_term not in self.term_dict:
+            raise ValueError(f"'{seed_term}' is not present in corpus!")
 
-        # your code here ...
+        current_word = seed_term
+        generated = [current_word]
 
-        return None
+        for _ in range(term_count - 1):
+            next_words = self.term_dict.get(current_word)
+
+            if not next_words:  # last word case
+                current_word = random.choice(list(self.term_dict.keys()))
+            else:
+                current_word = np.random.choice(next_words)
+
+            generated.append(current_word)
+
+        return " ".join(generated)
