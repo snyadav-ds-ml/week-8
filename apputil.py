e76ea8942a471388cf322de13033ddee69666c9c
Sneha Yadav <snyadav@iu.edu>
2025-10-22 22:30:57 +0000

    gs fix

--- apputil.py
+++ apputil.py
@@ -35,7 +35,8 @@ class MarkovText(object):
             next_words = self.term_dict.get(current_word)
 
             if not next_words:  # last word case
-                current_word = random.choice(list(self.get_term_dict.keys()))
+                current_word = random.choice(list(self.get_term_dict().keys()))
+                print(current_word)
             else:
                 current_word = np.random.choice(next_words)
 
